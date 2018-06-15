259c09c3f593a9ae5ed39327ba8b469a65f51ef6
bluedev83 <bludev83@outlook.com>
2018-06-14 20:56:58 -0400

    Commit to new branch with file path fix

--- Lecture_8_Execptions_Assertions/readingData.py
+++ Lecture_8_Execptions_Assertions/readingData.py
@@ -5,20 +5,18 @@ Created on Thu Jun  9 13:36:46 2016
 @author: WELG
 """
 
-data = []
+data44 = []
 
 file_name = input("Provide the name of a file of data ")
 
 try:
     fh = open(file_name, 'r')
 except IOError:
-    print('cannot open', file_name)
+    print('cannotTT open', file_name)
 else:
     for new in fh:
         if new != '\n':
-            addIt = new[:-1].split(',') #remove trailing \n
-            data.append(addIt)
+            addIt = new[:-1].split(',')  # remove trailing \n
+            data44.append(addIt)
 finally:
-    fh.close() # close file even if fail
-
-
+    fh.close()  # close file even if fail
